88c40da345ec4cc05863f4a2b79705eaafe72aa5
marynganga <marynganga.mw@gmail.com>
2017-08-23 19:44:35 +0300

    Create folder structure and input content into index.html, flashcard.css and flashcard.js.

---
+++ js/flashcard.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+  $("#javascript").click(function() {
+    $("#javascript-definition").toggle();
+  });
+
+  $("#operators").click(function() {
+    $("#operators-definition").toggle();
+  });
+
+  $("#variables").click(function() {
+    $("#variables-definition").toggle();
+  });
+
+  $("#variable-naming").click(function() {
+    $("#variable-naming-conventions").toggle();
+  });
+
+  $("#functions").click(function() {
+    $("#functions-definition").toggle();
+  });
+
+  $("#strings").click(function() {
+    $("#strings-definition").toggle();
+  });
+
+  $("#methods").click(function() {
+    $("#methods-definition").toggle();
+  });
+
+  $("#arguments").click(function() {
+    $("#arguments-definition").toggle();
+  });
+
+  $("#parameters").click(function() {
+    $("#parameters-definition").toggle();
+  });
+
+  $("#return").click(function() {
+    $("#return-definition").toggle();
+  });
+
+  $("#chaining-methods").click(function() {
+    $("#chaining-methods-definition").toggle();
+  });
+
+  $("#booleans").click(function() {
+    $("#booleans-definition").toggle();
+  });
+
+  $("#undefined").click(function() {
+    $("#undefined-definition").toggle();
+  });
+
+  $("#nan").click(function() {
+    $("#nan-definition").toggle();
+  });
+
+  $("#escape").click(function() {
+    $("#escape-definition").toggle();
+  });
+
+  $("#alert").click(function() {
+    $("#alert-definition").toggle();
+  });
+
+  $("#comments").click(function() {
+    $("#comments-definition").toggle();
+  });
+
+  $("#jQuery").click(function() {
+    $("#jQuery-definition").toggle();
+  });
+
+  $("#attributes").click(function() {
+    $("#attributes-definition").toggle();
+  });
+
+});
